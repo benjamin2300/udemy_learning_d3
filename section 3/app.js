42708327210914efe88366627532f48e2b641eb1
finch <finch2300@yahoo.com>
2019-12-20 15:51:01 +0800

    first commit, starting from section 4-5

---
+++ section 3/app.js
@@ -0,0 +1,31 @@
+var dataset = [10, 20, 30, 40, 50]
+
+var el = d3.select('body')
+           .selectAll( 'p' )
+           .data( dataset )
+           .enter()
+           .append( 'p' )
+           .text( function(d){
+             return "add new line through function " + d
+           })
+          //  .append("p")
+           .attr("class", function(d){
+             if(d>25){
+               return "foo";
+             }else{
+               return null;
+             }
+           })
+          //  .text("Hello World")
+           .style("color", function(d){
+             if(d>25){
+               return "red";
+             }else{
+               return "blue";
+             }
+           })
+          //  .classed("foo", true)
+           .classed("bar", function(d){
+             return d<25;
+           });
+console.log(el);
---
+++ section 4/app.js
@@ -0,0 +1,52 @@
+var data = [];
+
+for(var i=0; i<5; i++){
+  // Math.round()
+  // var num = Math.floor(Math.random() * 50);
+  var num = Math.floor(d3.randomUniform(1, 50)());
+  data.push(num);
+}
+
+console.log(data);
+var chart_width = 800;
+var chart_height = 400;
+var bar_padding = 5;
+
+var svg = d3.select('#chart')
+            .append('svg')
+            .attr('width', chart_width)
+            .attr('height', chart_height);
+
+svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', function(d, i){
+    return i * (chart_width / data.length);
+  })
+  .attr('y', function(d){
+    return chart_height - d*5
+  })
+  .attr('width', chart_width / data.length - bar_padding)
+  .attr('height', function(d){
+    return d * 5;
+  })
+  .attr('fill', '#7ed26d');
+
+  svg.selectAll('text')
+     .data(data)
+     .enter()
+     .append('text')
+     .text(function(d){
+       return d;
+     })
+     .attr('x', function(d, i){
+      return i * (chart_width / data.length) +
+                 (chart_width / data.length - bar_padding) / 2;
+     })
+     .attr('y', function(d){
+       return chart_height - d*5 + 15;
+     })
+     .attr('font.size', 14)
+     .attr('fill', '#fff')
+     .attr('text-anchor', 'middle');
